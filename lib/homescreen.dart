2af3ebebc45d0e258370c25f604cf9052821d046
alffy007 <jamesbond006aj@gmail.com>
2023-11-11 18:10:44 +0530

    spp

---
+++ lib/homescreen.dart
@@ -0,0 +1,243 @@
+import 'dart:io';
+
+import 'package:file_manager/file_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String directoryPath =
+      '/storage/emulated/0/MIUI/sound_recorder/call_rec/';
+  Future<File> getMostRecentFile(String directoryPath) async {
+    Directory directory = Directory(directoryPath);
+
+    if (!directory.existsSync()) {
+      throw "Directory not found: $directoryPath";
+    }
+
+    List<FileSystemEntity> files = directory.listSync();
+
+    if (files.isEmpty) {
+      throw "No files found in the directory: $directoryPath";
+    }
+
+    files
+        .sort((a, b) => b.statSync().modified.compareTo(a.statSync().modified));
+
+    return files.first as File;
+  }
+
+  void _fetchAndPrintMostRecentFile() async {
+    try {
+      File mostRecentFile = await getMostRecentFile(directoryPath);
+      print('Most recent file: ${mostRecentFile.path}');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+    // Make New Function
+    void _listofFiles() async {
+        directory = (await getApplicationDocumentsDirectory()).path;
+        setState(() {
+          file = io.Directory("$directory/resume/").listSync();  //use your folder name insted of resume.
+        });
+      }
+      
+  @override
+  void initState() {
+     _listofFiles();// TODO: implement initState
+    super.initState();
+  }
+  // Make New Function
+    void _listofFiles() async {
+        directory = (await getApplicationDocumentsDirectory()).path;
+        setState(() {
+          file = io.Directory("$directory/resume/").listSync();  //use your folder name insted of resume.
+        });
+      }
+  final FileManagerController controller = FileManagerController();
+  @override
+  Widget build(BuildContext context) {
+    return ControlBackButton(
+        controller: controller,
+        child: Scaffold(
+          body: Center(
+            child: FloatingActionButton(
+              onPressed: _fetchAndPrintMostRecentFile,
+              child: const Icon(Icons.get_app),
+            ),
+          ),
+        ));
+  }
+
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      actions: [
+        IconButton(
+          onPressed: () => createFolder(context),
+          icon: const Icon(Icons.create_new_folder_outlined),
+        ),
+        IconButton(
+          onPressed: () => sort(context),
+          icon: const Icon(Icons.sort_rounded),
+        ),
+        IconButton(
+          onPressed: () => selectStorage(context),
+          icon: const Icon(Icons.sd_storage_rounded),
+        )
+      ],
+      title: ValueListenableBuilder<String>(
+        valueListenable: controller.titleNotifier,
+        builder: (context, title, _) => Text(title),
+      ),
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () async {
+          await controller.goToParentDirectory();
+        },
+      ),
+    );
+  }
+
+  Widget subtitle(FileSystemEntity entity) {
+    return FutureBuilder<FileStat>(
+      future: entity.stat(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          if (entity is File) {
+            int size = snapshot.data!.size;
+
+            return Text(
+              "${FileManager.formatBytes(size)}",
+            );
+          }
+          return Text(
+            "${snapshot.data!.modified}".substring(0, 10),
+          );
+        } else {
+          return const Text("");
+        }
+      },
+    );
+  }
+
+  Future<void> selectStorage(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: FutureBuilder<List<Directory>>(
+          future: FileManager.getStorageList(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final List<FileSystemEntity> storageList = snapshot.data!;
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: storageList
+                        .map((e) => ListTile(
+                              title: Text(
+                                "${FileManager.basename(e)}",
+                              ),
+                              onTap: () {
+                                controller.openDirectory(e);
+                                Navigator.pop(context);
+                              },
+                            ))
+                        .toList()),
+              );
+            }
+            return const Dialog(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  sort(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                  title: const Text("Name"),
+                  onTap: () {
+                    controller.sortBy(SortBy.name);
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  title: const Text("Size"),
+                  onTap: () {
+                    controller.sortBy(SortBy.size);
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  title: const Text("Date"),
+                  onTap: () {
+                    controller.sortBy(SortBy.date);
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                  title: const Text("type"),
+                  onTap: () {
+                    controller.sortBy(SortBy.type);
+                    Navigator.pop(context);
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  createFolder(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController folderName = TextEditingController();
+        return Dialog(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  title: TextField(
+                    controller: folderName,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      // Create Folder
+                      await FileManager.createFolder(
+                          controller.getCurrentPath, folderName.text);
+                      // Open Created Folder
+                      controller.setCurrentPath =
+                          controller.getCurrentPath + "/" + folderName.text;
+                    } catch (e) {}
+
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Create Folder'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gather/homescreen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -6,15 +7,8 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomeScreen());
   }
 }
